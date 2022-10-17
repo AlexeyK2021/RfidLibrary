a09b6d266ae759760c1bca31142429496bf388ad
AlexeyK2021 <kalashnickov.alexey2015@yandex.ru>
2022-10-17 18:49:17 +0300

    Blink

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-import os
+import time
+
+from RPi import GPIO
+
+
+def blink():
+    GPIO.output(7, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(7, GPIO.LOW)
+    time.sleep(1)
+
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.OUT)
+    GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+def loop():
+    blink()
+    if GPIO.input(11):
+        return False
+    return True
+
+
+def reset():
+    GPIO.cleanup()
 
 
 if __name__ == "__main__":
-    print("Hello World")
-    print(os.name, os.uname())
+    setup()
+    isWork = True
+    while isWork:
+        isWork = loop()
+    reset()
